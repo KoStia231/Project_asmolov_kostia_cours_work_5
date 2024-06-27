7f4fd4e0ae18ca5c4dd88c4c0c9e56ec6922b6a4
konstantin <kosta123139@gmail.com>
2024-06-27 22:12:31 +0300

    base api

---
+++ src/api_clients/base.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+import requests
+
+
+class ApiClient(ABC):
+    @property
+    @abstractmethod
+    def base_url(self) -> str:
+        pass
+
+    def _get(self, url: str, params: dict) -> dict:
+        full_url = self.base_url + url
+        response = requests.get(full_url, params=params, timeout=10)
+        response.raise_for_status()
+        return response.json()
