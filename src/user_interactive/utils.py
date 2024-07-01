45fe5e2559885c0000e20d48d4d20722b855bad1
konstantin <kosta123139@gmail.com>
2024-07-01 21:04:05 +0300

    финиш

--- src/user_interactive/utils.py
+++ src/user_interactive/utils.py
@@ -15,7 +15,7 @@ print_user_choice = """
 print_user_choice_hh = """
 Выберите действие :
 1 - Поиск компании по ключевому слову
-2 - Поиск всех вакансий компании по id компании
+2 - Вывести информацию по id компании
 q - возврат в предыдущее меню
 """
 
@@ -37,7 +37,7 @@ def search_company_name():
             client = HHClients()
             results = client.employers_search(name)
             table = PrettyTable()
-            table.field_names = ['id', 'name', 'url', 'open_vacancies']
+            table.field_names = ['id', 'Компания', 'Компания на hh.ru', 'Открытые вакансии']
             for result in results:
                 table.add_row([result.id, result.name, result.url, result.open_vacancies])
             print(table)
@@ -54,7 +54,11 @@ def id_company():
             client = HHClients()
             results = client.get_employer_info(int(name))
             table = PrettyTable()
-            table.field_names = ['id', 'name', 'url', 'site_url', 'region', 'open_vacancies']
+            table.field_names = ['id', 'Компания',
+                                 'Компания на hh.ru',
+                                 'Ссылка на сайт компании',
+                                 'Регион',
+                                 'Открытые вакансии']
             table.add_row(
                 [results.id, results.name, results.url, results.site_url, results.region, results.open_vacancies])
             print(table)
@@ -89,13 +93,13 @@ def job_bd():
             break
         elif user_input == '1':
             data = data_base.get_companies_and_vacancies_count()
-            table.field_names = ['Компания', 'количество вакансий']
+            table.field_names = ['Компания', 'Количество вакансий']
             for d in data:
                 table.add_row([d[0], d[1]])
             print(table)
         elif user_input == '2':
             data = data_base.get_all_vacancies()
-            table.field_names = ['компания', 'вакансия', 'зарплата от', 'зарплата до', 'ссылка']
+            table.field_names = ['Компания', 'Вакансия', 'Зарплата от', 'Зарплата до', 'Ссылка']
             for d in data:
                 table.add_row([d[0], d[1], d[2], d[3], d[4]])
             print(table)
@@ -104,15 +108,15 @@ def job_bd():
             print(f'Средняя зарплата по всем вакансиям: {data} руб.')
         elif user_input == '4':
             data = data_base.get_vacancies_with_higher_salary()
-            table.field_names = ['вакансия', 'зарплата от', 'зарплата до', 'ссылка']
+            table.field_names = ['Вакансия', 'Зарплата от', 'Зарплата до', 'Ссылка']
             for d in data:
                 table.add_row([d[0], d[1], d[2], d[3]])
             print(table)
         elif user_input == '5':
-            print('введите ключевое слово:')
+            print('Введите ключевое слово:')
             name = input()
             data = data_base.get_vacancies_with_keyword(name)
-            table.field_names = ['вакансия', 'зарплата от', 'зарплата до', 'ссылка']
+            table.field_names = ['Вакансия', 'Зарплата от', 'Зарплата до', 'Ссылка']
             for d in data:
                 table.add_row([d[0], d[1], d[2], d[3]])
             print(table)
